fcae621c31d969288224d6893a6929d0e95a99a8
Monikha-K <monikha.k2024cse@sece.ac.in>
2025-12-16 00:38:59 +0530

    Added Classical Dance page

---
+++ Classical.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DancePageTemplate from './DancePageTemplate';
+
+const Classical = () => {
+    return <DancePageTemplate danceId="classical" />;
+};
+
+export default Classical;
