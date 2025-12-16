77e8c25396577ef3b99c61671d05fe68be44e8dc
PAVITHRA M <pavithra.m2024cse@sece.ac.in>
2025-12-16 09:03:08 +0530

    This is my commit
    This commit contains the hiphop and Western Code Folder.

---
+++ HipHop.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DancePageTemplate from './DancePageTemplate';
+
+const HipHop = () => {
+    return <DancePageTemplate danceId="hiphop" />;
+};
+
+export default HipHop;
---
+++ Western.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DancePageTemplate from './DancePageTemplate';
+
+const Western = () => {
+    return <DancePageTemplate danceId="western" />;
+};
+
+export default Western;
