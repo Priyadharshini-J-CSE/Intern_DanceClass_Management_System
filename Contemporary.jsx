85eb6d4eee8b37592f68ce8671569f4a250b6f83
PRIYA-P14 <priya.p2024cse@sece.ac.in>
2025-12-16 09:05:33 +0530

    Zumba and Contemporary Dances

---
+++ Contemporary.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DancePageTemplate from './DancePageTemplate';
+
+const Contemporary = () => {
+    return <DancePageTemplate danceId="contemporary" />;
+};
+
+export default Contemporary;
---
+++ Zumba.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DancePageTemplate from './DancePageTemplate';
+
+const Zumba = () => {
+    return <DancePageTemplate danceId="zumba" />;
+};
+
+export default Zumba;
